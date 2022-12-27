134e09759caa80ccf0c57f6f5524231a7c4c3521
Igão <igaosouza@Igaos-MacBook-Pro.local>
2022-12-27 16:39:08 -0300

    chore: developer configs

---
+++ src/styles/globals.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body, #__next {
+    font-family: 'Unbounded', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: space-between;
+  }
+  body {
+    min-height: 100vh;
+    min-width: 100vw;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+  #__next {
+    max-width: 1440px;
+  }
+`;
